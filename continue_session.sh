9af56174109b96ced5edcda2a442f7ed95407ec8
Ton Nom <ton_email_github@exemple.com>
2025-10-18 12:45:38 +0200

    chore: Remove session continuation script

--- continue_session.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Continuer la session de débogage.
-# Étape actuelle : Valider que les tests passent après les corrections.
-
-# Nettoyer le cache de pytest pour éviter les erreurs d'importation périmées
-pytest --cache-clear
-
-# Lancer les tests
-pytest
